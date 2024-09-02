ac0e004c2e06693316614ba9e81608cc8925246d
A-lpha18 <163670986+A-lpha18@users.noreply.github.com>
2024-09-03 00:33:47 +0530

    added index.html

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function rollDice(){
-    randomdice1=Math.floor(Math.random()*6)+1;
+    var randomdice1=Math.floor(Math.random()*6)+1;
+    // var randomImage="./images"+randomdice1+"png";
+    document.getElementById('firstdice').src="./images/dice"+randomdice1+".png";
     console.log(randomdice1);
+    var randomdice2=Math.floor(Math.random()*6)+1;
+    // var randomImage="./images"+randomdice1+"png";
+    console.log(randomdice2);
+    document.getElementById('seconddice').src="./images/dice"+randomdice2+".png";
+    if(randomdice1>randomdice2){
+        document.querySelector("#head").innerHTML="Player 1 WON!";
+    }
+    else if(randomdice1<randomdice2){
+        document.querySelector("#head").innerHTML="Player 2 WON!";
+    }
+    else{
+        document.querySelector("#head").innerHTML="Draw!";
+    }
 }
